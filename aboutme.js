313744c6c5100e6b75b89aacc518f316babafc90
Noriishere <noriyamashin@gmail.com>
2025-11-26 00:40:11 +0700

    patch 2

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,5 @@
 export const aboutme= [{
     title: "About Me",
     description: "My name is Bagas Nurdiansyah, but people usually call me Bagas. I’m a curious learner who loves diving into technology, design, and creative development. I enjoy building things from scratch, experimenting with new ideas, and sharpening my skills through real projects. For me, learning isn’t just a task it's a journey I genuinely enjoy. Lately, I’ve been developing a strong passion for React.js and continuously pushing myself toward becoming a full-stack developer, both in PHP and the MERN Stack.",
-    side: "Contact me or scroll down to see my projects. I can handling html, css and javascript Program, have skill at leadership and self motivation"
+    side: "Feel free to reach out or scroll down to explore my projects. I work confidently with HTML, CSS, and JavaScript, and I’m driven by strong leadership and self-motivation."
 }]
