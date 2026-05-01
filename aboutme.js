1dcbe8024df28d46c670226a5ff3747222a9e028
Noriishere <noriyamashin@gmail.com>
2026-05-01 23:53:14 +0700

    Feat: Rework

--- aboutme.js
+++
@@ -1,5 +0,0 @@
-export const aboutme= [{
-    title: "About Me",
-    description: "My name is Bagas Nurdiansyah, but people usually call me Bagas. I’m a curious learner who loves diving into technology, design, and creative development. I enjoy building things from scratch, experimenting with new ideas, and sharpening my skills through real projects. For me, learning isn’t just a task it's a journey I genuinely enjoy. Lately, I’ve been developing a strong passion for React.js and continuously pushing myself toward becoming a full-stack developer, both in PHP and the MERN Stack.",
-    side: "Feel free to reach out or scroll down to explore my projects. I work confidently with HTML, CSS, and JavaScript, and I’m driven by strong leadership and self-motivation."
-}]
